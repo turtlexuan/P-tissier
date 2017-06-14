9dea5b9be0a975f258aba2dacf38fca09c6d6867
turtlexuan <turtlexuan@gmail.com>
2017-06-14 14:44:38 +0800

    1. Create project.

---
+++ Pâtissier/Pâtissier/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  Pâtissier
+//
+//  Created by 劉仲軒 on 2017/6/14.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    weak var productImageView = UIImageView()
+    weak var productBackground = UIView()
+    weak var productTitleLabel = UILabel()
+    
+    
+}
